6b09fc4f3f45db882473d85a8ed9cc6aed8bcc2f
ratchakoonprn <ratchakoon.prn@outlook.com>
2020-07-28 09:31:57 +0700

    Modify Read Write File Sync

--- 01/readwritefile.js
+++ 01/readwritefile.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
+
+// Read File
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
+
+// Write File
+const textOut = `Avocado information: ${textIn}\nCreat on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written');
