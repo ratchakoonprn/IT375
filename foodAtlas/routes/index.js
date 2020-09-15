cd70de13ccb54daaded262f9fe0ab6c13435f40b
ratchakoonprn <ratchakoon.prn@outlook.com>
2020-09-15 14:37:24 +0700

    Edit Index.html

--- foodAtlas/routes/index.js
+++ foodAtlas/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express to Heroku' });
 });
 
 // router.get('/restaurants', function(req, res, next) {
